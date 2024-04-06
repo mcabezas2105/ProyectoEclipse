702eee9c1d922ffa3a998459ecc6cc0b6a93e12b
Usuario <mcabezasbanegas@gmail.com>
2024-04-06 10:36:35 +0200

    GetAndSetC

--- src/proyectoEclipse/ClaseC.java
+++ src/proyectoEclipse/ClaseC.java
@@ -7,4 +7,12 @@ public ClaseC(String nombre) {
 	this.nombre = nombre;
 }
 
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
 }
